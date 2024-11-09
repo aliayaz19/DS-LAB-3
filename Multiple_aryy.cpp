a000ff3d4e7cf5ce3b301ab2e7910c7774686ccc
Muhammad Ali <151754816+aliayaz19@users.noreply.github.com>
2024-11-09 12:07:27 +0500

    Create Multiple_aryy.cpp

---
+++ Multiple_aryy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x[3][2] = {{0,1}, {2,3}, {4,5}}; // 3 rows 2 columns 
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 2; j++){
+            cout << "element at x [" << i << "][" << j << "]: ";
+            cout << x[i][j] << endl;
+        }
+    }
+}
